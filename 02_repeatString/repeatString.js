48895faec8e5702339e6a2102b88629d2724d8c7
the-soban <sobanshafeq123@gmail.com>
2023-07-13 15:04:24 +0500

    completed 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (word, repeats) {
+    if (repeats < 0) {
+        return "ERROR";
+    }
+    let string = "";
+    for (let i = 0; i < repeats; i++) {
+        string += word;
+    }
+    return string;
 };
+
+repeatString('hola', 6);
 
 // Do not edit below this line
 module.exports = repeatString;

fceeff3ac240a0e7e3fd1f6c39109acd9f6713eb
the-soban <sobanshafeq123@gmail.com>
2023-07-13 15:14:24 +0500

    completed 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let splitString = string.split(""); //splits the string into an array
+    let reversedString = splitString.reverse(); //reverses the array
+    let joinString = reversedString.join(""); //joins the reversed array's all elements
+    // console.log(joinString);
+    return joinString;
 };
 
+reverseString("hello there!");
 // Do not edit below this line
 module.exports = reverseString;
